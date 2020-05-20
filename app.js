75b048ab3bf290c26648f9417759f33b53f2dec5
Elisabeth <elisabethvirak@gmail.com>
2020-05-20 13:08:39 -0400

    Reorganized code

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
-// @TODO: YOUR CODE HERE!
+//initialize SVG area
+var svgWidth = 1000;
+var svgHeight = 700;
+
+// define margins for the SVG
+var chartMargin = {
+    top:30,
+    right: 30,
+    bottom: 30,
+    left: 30
+};
+
+// define area for chart
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// add SVG to the webpage
+var svg = d3.select('#scatter')
+    .append('svg')
+    .attr('height', svgHeight)
+    .attr('width', svgWidth);
+
+// add chart area to the SVG
+var chartGroup = svg.append('g')
+    .attr('transform', `translate(${chartMargin.left}, ${chartMargin.top})`);
+    
 d3.csv("data.csv").then(data =>{
     console.log(data)
-     
     // create variables
     var abbr = data.map(data => data.abbr);
     var poverty = data.map(data => data.poverty);
@@ -11,29 +35,5 @@ d3.csv("data.csv").then(data =>{
     var obesity = data.map(data => data.obesity);
     var smokes = data.map(data => data.smokes);
 
-    //initialize SVG area
-    var svgWidth = 1000;
-    var svgHeight = 700;
-
-    // define margins for the SVG
-    var chartMargin = {
-        top:30,
-        right: 30,
-        bottom: 30,
-        left: 30
-    };
-
-    // define area for chart
-    var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
-    var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
-
-    // add SVG to the webpage
-    var svg = d3.select('#scatter')
-        .append('svg')
-        .attr('height', svgHeight)
-        .attr('width', svgWidth);
-    
-    // add chart area to the SVG
-    var chartGroup = svg.append('g')
-        .attr('transform', `translate(${chartMargin.left}, ${chartMargin.top})`);
+            
 });
